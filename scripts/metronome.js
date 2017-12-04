9a695bd049f292d85256438688a43e918ce2299d
Ethan Lipkind <ethan.lipkind@gmail.com>
2017-12-03 21:42:54 -0500

    added pause button, cleaned up css

--- scripts/metronome.js
+++ scripts/metronome.js
@@ -1,26 +1,34 @@
 $(document).ready(function(){
-	var slider = $('.slider');
-	var tempo = $('#tempo');
-	var click = $('#click')[0];
-	var play = $('#play');
+	var $slider = $('.slider');
+	var $tempo = $('#tempo');
+	var $click = $('#click')[0];
+	var $play = $('#play');
+	var $pause = $('#pause');
 	var t = 600; // initial value = 100bpm = 1 beat per 600 ms
 	var playing = false;
 	var start;
 
-	tempo.text(slider.val() + ' bpm');
+	$tempo.text($slider.val() + ' bpm');
 
-	slider.on('input', function() {
-		tempo.text(this.value + ' bpm');
+	$slider.on('input', function() {
+		console.log(this.value);
+		$tempo.text(this.value + ' bpm');
 		t = 60000 / this.value;
 	});
 
-	play.on('click', function(){
+	$play.on('click', function(){
 		if(!playing){
 			console.log('playing');
-			start = setInterval(function(){click.play();}, t);
+			start = setInterval(function() { 
+				click.play();
+			}, t);
 			playing = true;
 		}
-		else{
+		
+	});
+
+	$pause.on('click', function(){
+		if(playing) {
 			console.log('pausing');
 			clearInterval(start);
 			playing = false;
